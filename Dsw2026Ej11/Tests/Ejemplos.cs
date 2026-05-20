6edb2f980b897aaf58de891558529fbce5f13810
Tomas Recalde <tomasrecalde99@gmail.com>
2026-05-20 15:42:26 -0300

    Clase CasoList completado

--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -11,7 +14,50 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        CasoList gestor = new CasoList();
 
+        Alumno alumno1 = new Alumno(101, "Selena", 8.5);
+        Alumno alumno2 = new Alumno(102, "Carlos", 7.3);
+        Alumno alumno3 = new Alumno(103, "Miguel", 6.8);
+
+        gestor.Agregar(alumno1);
+        gestor.Agregar(alumno2);
+        gestor.Agregar(alumno3);
+
+        Console.WriteLine("--- Lista de Alumnos Inicial ---");
+        foreach (Alumno alu in gestor.ObtenerLista())
+        {
+            Console.WriteLine($"Nombre: {alu.Nombre} | Id: {alu.Id} | Promedio: {alu.Promedio}");
+        }
+
+        Console.WriteLine("\n--- Buscando a Miguel ---");
+        Alumno encontrado = gestor.BuscarAlumnoPorNombre("Miguel");
+        if (encontrado != null)
+        {
+            Console.WriteLine($"El alumno es: {encontrado.Nombre}");
+        }
+
+        Console.WriteLine("\n--- Buscando a Juan ---");
+        Alumno noEncontrado = gestor.BuscarAlumnoPorNombre("Juan");
+        if (noEncontrado == null) 
+        {
+            Console.WriteLine("No existe");
+        }
+
+        Console.WriteLine("\n--- Eliminando a Miguel ---");
+        gestor.RemoverAlumno(alumno3);
+
+        foreach (Alumno alu in gestor.ObtenerLista())
+        {
+            Console.WriteLine($"Nombre: {alu.Nombre}");
+        }
+
+        Console.WriteLine("\n--- Eliminando el primer elemento (Posición 0) ---");
+        gestor.EliminarPorPosicion(0);
+        foreach (Alumno alu in gestor.ObtenerLista())
+        {
+            Console.WriteLine($"Nombre: {alu.Nombre}");
+        }
     }
 
     //Agregar 3 alumnos al diccionario
